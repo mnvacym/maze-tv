fd6644f23f6b9a197d27aa9ae7e17562dd2364bb
Ammar Merakli <merakli.vehbi@gmail.com>
2022-06-23 22:40:57 +0200

    ci(general): configure ngneat spectator for unit testing

--- apps/tv-shows/src/app/app.component.spec.ts
+++ apps/tv-shows/src/app/app.component.spec.ts
@@ -1,31 +1,23 @@
-import { TestBed } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
+import { createComponentFactory, Spectator } from '@ngneat/spectator/jest';
+
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [AppComponent]
-    }).compileComponents();
+  let spectator: Spectator<AppComponent>;
+
+  const createComponent = createComponentFactory({
+    component: AppComponent,
+    imports: [RouterTestingModule],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    declarations: [AppComponent],
+    detectChanges: false
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+  beforeEach(() => (spectator = createComponent()));
 
-  it(`should have as title 'tv-shows'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('tv-shows');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome to tv-shows!'
-    );
+  it('should create', () => {
+    expect(spectator.component).toBeTruthy();
   });
 });
